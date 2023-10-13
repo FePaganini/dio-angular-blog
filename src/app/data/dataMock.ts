698a932cad90c4b0584722b47e05139f8e9626d5
FePaganini <felipespaganini@hotmail.com>
2023-10-13 17:22:42 -0300

    Feat: add data mock to show in cards

---
+++ src/app/data/dataMock.ts
@@ -0,0 +1,16 @@
+export const dataMock = [
+  {
+    id: '1',
+    imgUrl:
+      'https://pbs.twimg.com/media/Fzp7fLHXsAUN8U_?format=jpg&name=4096x4096',
+    title: 'Chopper No L.A',
+    description: 'Ele chegará na segunda temporada',
+  },
+  {
+    id: '2',
+    imgUrl:
+      'https://pbs.twimg.com/media/Fzp7fLHXsAUN8U_?format=jpg&name=4096x4096',
+    title: 'Chopper No L.A',
+    description: 'Ele chegará na segunda temporada',
+  },
+];
