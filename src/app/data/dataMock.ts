7216e9bbc753b4b57dcee1cec3b75ca2bbcd3e3e
FePaganini <felipespaganini@hotmail.com>
2023-10-21 00:08:39 -0300

    Feat: complete course of dio

--- src/app/data/dataMock.ts
+++ src/app/data/dataMock.ts
@@ -2,9 +2,9 @@ export const dataMock = [
   {
     id: '1',
     imgUrl:
-      'https://pbs.twimg.com/media/Fzp7fLHXsAUN8U_?format=jpg&name=4096x4096',
-    title: 'Chopper No L.A',
-    description: 'Ele chegará na segunda temporada',
+      'https://s2-techtudo.glbimg.com/xUdMvOgAGqAAF0hUEHQuSgKXNUw=/0x0:1370x771/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/b/K/FjBQz6Rl2IQl0V7evYKw/arcane-final-poster-16x9-no-text-no-border.jpg',
+    title: '2ª Temporada de Arcane',
+    description: 'Cadê isso? Pelo amor de Deus',
   },
   {
     id: '2',
@@ -12,5 +12,19 @@ export const dataMock = [
       'https://pbs.twimg.com/media/Fzp7fLHXsAUN8U_?format=jpg&name=4096x4096',
     title: 'Chopper No L.A',
     description: 'Ele chegará na segunda temporada',
+  },
+  {
+    id: '3',
+    imgUrl:
+      'https://www.leagueoflegends.com/static/open-graph-b580f0266cc3f0589d0dc10765bc1631.jpg',
+    title: 'Motivos para jogar LoL',
+    description: 'Descubra os motivos de LoL ser jogado até hoje!',
+  },
+  {
+    id: '4',
+    imgUrl:
+      'https://t.ctcdn.com.br/X1n4qStmYomk4a5_B_crIbvmDK0=/1024x0/smart/i793063.jpeg',
+    title: 'O Chopper é muito fofo',
+    description: 'Apenas o futuro melhor médico do mundo',
   },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { dataMock } from 'src/app/data/dataMock';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  bigCardData = dataMock[0];
+  smallCardsData = dataMock.slice(1);
   constructor() {}
 
   ngOnInit(): void {}
